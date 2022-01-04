ae73c2968b080e8815fa9656ed76430c3f5d9ab0
kmtszky <79302477+kmtszky@users.noreply.github.com>
2022-01-04 20:27:57 +0900

    [update] よりわかりやすくコードを修正

--- type90_084/thereAreTwoTypesOfCharacters.go
+++ type90_084/thereAreTwoTypesOfCharacters.go
@@ -13,14 +13,13 @@ func main() {
 	var s string
 	fmt.Fscan(in, &n, &s)
 
-	// o = 111, x=120
 	A := make([]int, n+1)
 	B := make([]int, n+1)
 	for i := 1; i < n+1; i++ {
-		if s[i-1] == 111 {
+		if s[i-1] == 'o' {
 			A[i] = i
 			B[i] = B[i-1]
-		} else if s[i-1] == 120 {
+		} else if s[i-1] == 'x' {
 			A[i] = A[i-1]
 			B[i] = i
 		}
